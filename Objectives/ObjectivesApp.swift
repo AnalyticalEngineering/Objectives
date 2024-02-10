f937d007b31bfb82f26645ac9598c0ea90b063c0
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-09 18:43:48 -0600

    Initial Commit

---
+++ Objectives/ObjectivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectivesApp.swift
+//  Objectives
+//
+//  Created by J. DeWeese on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ObjectivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
